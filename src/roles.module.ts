282bbf0e867f6e9cf237a0d768b251ddda2fd293
JcJet <jcjet88@gmail.com>
2023-05-12 02:58:59 +0300

    database isolation

--- src/roles.module.ts
+++ src/roles.module.ts
@@ -4,24 +4,35 @@ import { RolesService } from './roles.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Role } from './roles.entity';
-import { UserRoles } from "./roles_users.entity";
+import { UserRoles } from './roles_users.entity';
 
+const databaseHost = process.env.DB_HOST || 'localhost';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env',
+      //envFilePath: '.env',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
+      host: databaseHost,
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      database: 'roles',
+      entities: [Role, UserRoles],
+      synchronize: true,
+    }),
+/*    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD.toString(),
       database: process.env.POSTGRES_DB,
       entities: [Role, UserRoles],
       synchronize: true,
-    }),
+    }),*/
     TypeOrmModule.forFeature([Role, UserRoles]),
   ],
   controllers: [RolesController],
