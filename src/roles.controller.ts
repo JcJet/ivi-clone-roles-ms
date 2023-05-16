cae7dc34b257b718c73090ed7444ccc9f5524e10
JcJet <jcjet88@gmail.com>
2023-05-17 01:36:57 +0300

    ms-compatibility

--- src/roles.controller.ts
+++ src/roles.controller.ts
@@ -2,42 +2,41 @@ import { Controller } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { RoleDto } from './dto/role.dto';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import { AddUserRoleRecordDto } from "./dto/addUserRoleRecord.dto";
 import { UpdateUserRoleDto } from "./dto/updateUserRole.dto";
 
 @Controller()
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
-  @MessagePattern('createRole')
+  @MessagePattern({ cmd: 'createRole' })
   async createRole(@Payload() data: { dto: RoleDto }) {
     return await this.rolesService.createRole(data.dto);
   }
-  @MessagePattern('getRoleById')
+  @MessagePattern({ cmd: 'getRoleById' })
   async getRoleById(@Payload() data: { id: number }) {
     return await this.rolesService.getRoleById(data.id);
   }
-  @MessagePattern('getAllRoles')
+  @MessagePattern({ cmd: 'getAllRoles' })
   async getAllRoles() {
     return await this.rolesService.getAllRoles();
   }
-  @MessagePattern('updateRole')
+  @MessagePattern({ cmd: 'updateRole' })
   async updateRole(@Payload() data: { id: number; dto: RoleDto }) {
     return await this.rolesService.updateRole(data.id, data.dto);
   }
-  @MessagePattern('deleteRoleByValue')
+  @MessagePattern({ cmd: 'deleteRoleByValue' })
   async deleteRoleByValue(@Payload() data: { value: string }) {
     return await this.rolesService.deleteRoleByValue(data.value);
   }
-  @MessagePattern('addUserRoles')
+  @MessagePattern({ cmd: 'addUserRoles' })
   async addUserRoles(@Payload() data: { dto: UpdateUserRoleDto }) {
     return await this.rolesService.addUserRoles(data.dto);
   }
-  @MessagePattern('getUserRoles')
+  @MessagePattern({ cmd: 'getUserRoles' })
   async getUserRoles(@Payload() data: { userId: number }) {
     return await this.rolesService.getUserRoles(data.userId);
   }
-  @MessagePattern('deleteUserRoles')
+  @MessagePattern({ cmd: 'deleteUserRoles' })
   async deleteUserRoles(@Payload() data: { dto: UpdateUserRoleDto }) {
     return await this.rolesService.deleteUserRoles(data.dto);
   }
